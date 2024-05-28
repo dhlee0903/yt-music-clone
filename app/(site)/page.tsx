a68a0d97fd2c52f2008f08619b4aca8dd47adf98
dhlee0903 <dhlee0903@wepnp.com>
2024-05-28 11:48:52 +0900

    error Message 구현

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -2,10 +2,12 @@ import { sleep } from '@/lib/utils'
 import React from 'react'
 
 const page = async () => {
-  await sleep(2000);
-  return (
-    <div>HomePage</div>
-  )
+  // await sleep(2000);
+  // throw new Error("my error");
+  console.log("before homgPage sleep ...");
+  await sleep(4000);
+  console.log("after homgPage sleep ...");
+  return <div>HomePage</div>;
 }
 
 export default page
