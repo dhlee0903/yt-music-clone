ce8319af4cca92e6af3605c6ebd8180cb74cfa4f
dhlee0903 <dhlee0903@wepnp.com>
2024-05-29 16:34:24 +0900

    logo

--- components/elements/Logo.tsx
+++ components/elements/Logo.tsx
@@ -1,9 +1,24 @@
-import React from 'react'
-
+"use client"
+import Image from "next/image";
+import React from "react";
+import { RxHamburgerMenu } from "react-icons/rx";
 const Logo = () => {
-  return (
-    <div>Logo</div>
-  )
-}
 
-export default Logo
+    const onClickLogo = () =>{
+        // home 이동
+
+    };
+
+  return (
+    <section className="flex flex-row gap-4 items-center">
+      <div>
+        <RxHamburgerMenu size={24} />
+      </div>
+      <div className="cursor-pointer" onClick={onClickLogo}>
+        <Image alt="logo" width={100} height={30} src={"/main-Logo.svg"} />
+      </div>
+    </section>
+  );
+};
+
+export default Logo;
