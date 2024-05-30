0dbfc751fe854a614826006f121d9301a5bcb66e
dhlee0903 <dhlee0903@wepnp.com>
2024-05-30 17:27:11 +0900

    sidebar navigator

--- components/elements/Navigator.tsx
+++ components/elements/Navigator.tsx
@@ -4,25 +4,26 @@ import { GoHome } from "react-icons/go";
 import { FiPlus, FiMusic, FiCompass } from "react-icons/fi";
 import { usePathname, useSearchParams } from "next/navigation";
 import Link from "next/link";
+import { cn } from "@/lib/utils";
 
 const Navigator = () => {
   const pathname = usePathname();
   const routes = useMemo(() => {
     return [
       {
-        icon: <GoHome size={20} />,
+        icon: <GoHome size={24} />,
         label: "홈",
         isActive: pathname === "/",
         href: "/",
       },
       {
-        icon: <FiCompass size={20} />,
+        icon: <FiCompass size={24} />,
         label: "둘러보기",
         isActive: pathname === "/explore",
         href: "/explore",
       },
       {
-        icon: <FiMusic size={20} />,
+        icon: <FiMusic size={24} />,
         label: "보관함",
         isActive: pathname === "/library",
         href: "/library",
@@ -32,17 +33,31 @@ const Navigator = () => {
 
   return (
     <div>
-      <section>
+      <section className="flex flex-col gap-2 p-4">
         {routes.map((route) => {
           return (
             <Link key={route.label} href={route.href}>
-              <div className="flex flex-row gap-4">                
+              <div
+                className={cn(
+                  "text-[16px] flex flex-row items-center gap-4 hover:bg-neutral-700 rounded-lg p-2",
+                  route.isActive && "bg-neutral-800"
+                )}
+              >
                 {route.icon}
                 <span>{route.label}</span>
               </div>
             </Link>
           );
         })}
+      </section>
+      <section className="px-6">
+        <div className="w-full h-[1px] bg-neutral-700"></div>
+      </section>
+      <section className="px-6">
+        <div className="hover:bg-neutral-600 cursor-pointer flex flex-row items-center bg-neutral-700 my-6 rounded-3xl p-2 font-[100] justify-center gap-2">          
+          <div><FiPlus size={24}></FiPlus></div>
+          <span>새 재생 목록</span>
+        </div>
       </section>
     </div>
   );
